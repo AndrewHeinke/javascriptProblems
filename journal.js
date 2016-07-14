3b865584e6016c28a0fe1b4c8f7198a2a534187d
Andrew <andrew.j.heinke@gmail.com>
2016-07-13 22:53:03 -0500

    adding journal project

---
+++ journal.js
@@ -0,0 +1,3 @@
+// Guestbook / Journal – A simple application that allows people to add comments or write journal entries. It can allow comments or not and timestamps for all entries. Could also be made into a shout box.
+
+//will need html page, jquery for dom manipulation, moment.js for time stuff. Could use charts.js to display some cool statistics about jounral entry
